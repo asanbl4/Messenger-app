461b0eea510f20c8c938b17bccb48f0c54de7a37
assanalibatyrkhan <asankek12@gmail.com>
2024-05-17 15:58:12 +0500

    Photo messages now can be sent

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import FirebaseDatabase
-
+import MessageKit
 
 final class DatabaseManager {
     
@@ -411,6 +411,25 @@ extension DatabaseManager {
                       let date = ChatViewController.dateFormatter.date(from: dateString) else {
                     return nil
                 }
+                var kind: MessageKind?
+                if type == "photo" {
+                    guard let imageUrl = URL(string: content),
+                          let placeHolder = UIImage(systemName: "plus") else {
+                        return nil
+                    }
+                    
+                    let media = Media(url: imageUrl,
+                                      image: nil,
+                                      placeholderImage: placeHolder,
+                                      size: CGSize(width: 300, height: 300))
+                    kind = .photo(media)
+                } else {
+                    kind = .text(content)
+                }
+                guard let finalKind = kind else {
+                    return nil
+                }
+                
                 let sender = Sender(PhotoURL: "",
                                     senderId: senderEmail,
                                     displayName: name)
@@ -418,7 +437,7 @@ extension DatabaseManager {
                 return Message(sender: sender,
                                messageId: messageId,
                                sentDate: date,
-                               kind: .text(content))
+                               kind: finalKind)
             })
             
             completion(.success(messages))
@@ -456,8 +475,10 @@ extension DatabaseManager {
                 message = messageText
             case .attributedText(_):
                 break
-            case .photo(_):
-                break
+            case .photo(let mediaItem):
+                if let targetUrlString = mediaItem.url?.absoluteString {
+                    message = targetUrlString
+                }
             case .video(_):
                 break
             case .location(_):
@@ -539,7 +560,7 @@ extension DatabaseManager {
                             
                             let updatedValue: [String: Any] = [
                                 "date": dateString,
-                                "is_read":false,
+                                "is_read": false,
                                 "message": message,
                             ]
                             var targetConversation: [String: Any]?
